63d81ffd3be0f9ade386ffef6f4c68f7764a2263
weiqlog@126.com <weiqlog@126.com>
2022-12-15 10:09:37 +0800

    first commit

---
+++ src/main/java/com/weiqlog/weiqlog/WeiqlogApplication.java
@@ -0,0 +1,13 @@
+package com.weiqlog.weiqlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiqlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiqlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weiqlog/weiqlog/WeiqlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weiqlog.weiqlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiqlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
